5643b6ea697de3d10f8a5fec808ae00b71f420b5
swanbaek <swanbaek@naver.com>
2026-02-11 17:42:29 +0900

    memo관련 라우터 분리해 재편성함

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from fastapi import FastAPI, Request,Form
+from fastapi import FastAPI, Request,Form   
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from todo.app import todo_router
 from jinja_ex.example_app import app as jinja2_app
+from memo.app import router as memo_router
 
 
 # venv 안에서 FastAPI 설치
@@ -11,6 +12,7 @@ from jinja_ex.example_app import app as jinja2_app
 
 app= FastAPI()
 
+templates= Jinja2Templates(directory="templates/")
 # static 폴더 제공
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
@@ -26,18 +28,10 @@ def read_root()->dict:
 # http://127.0.0.1:8000
 
 
-templates = Jinja2Templates(directory="templates")
 
-# templates 폴더의 "index.html" 응답
-@app.get("/memo", response_class=HTMLResponse)
-def read_memo(request: Request):
-    return templates.TemplateResponse("memo/index.html", {"request": request, "메모장": "여기는 메모장 페이지입니다."})
-# http://127.0.0.1:8000/memo
 
-@app.post("/memo", response_class=HTMLResponse)
-async def post_memo(request: Request, content: str = Form(...)):
-    return templates.TemplateResponse("memo/result.html", {"request": request, "content": content})
 
 app.include_router(todo_router)
+app.include_router(memo_router)
 
 app.mount("/jinja_ex", jinja2_app)
